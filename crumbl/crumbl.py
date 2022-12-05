c5d5680a75a6fff0e8088935cad3ec2f7f37bd9e
andrewdkennedy1 <andrew@Bortonfruit.com>
2022-12-05 01:58:54 -0800

    uglyfied

--- crumbl/crumbl.py
+++ crumbl/crumbl.py
@@ -1,48 +1,24 @@
-import aiohttp
-import asyncio
-from bs4 import BeautifulSoup
-from redbot.core import commands
-import discord
-
-class crumbl(commands.Cog):
-    """Gets all the cookies"""
-
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-
-        self.__url: str = "https://crumblcookies.com/nutrition"
-        self.__session = aiohttp.ClientSession()
-
-    def cog_unload(self) -> None:
-        if self.__session:
-            asyncio.get_event_loop().create_task(self.__session.close())
-
-    @commands.command()
-    async def crumbl(self, ctx: commands.Context) -> None:
-        """Gets all the cookies"""
-
-        await ctx.trigger_typing()
-
-        try:
-            async with self.__session.get(self.__url) as response:
-                rawingredients = await response.text()
-                soup = BeautifulSoup(rawingredients, "html.parser")
-                cookies = soup.find_all('div', class_="bg-white p-5 pb-0 mb-2.5 rounded-lg")
-                for b in cookies:
-                    titles = b.find_all("b", {"class": "text-lg"})
-                    desc = b.find_all("p", {"class": "text-sm"})
-                    thumb_url = b.find_all('img', {"class": "object-contain"})
-                    contains = b.find_all("span", {"class": "flex items-center justify-center"})
-                    for b in titles:
-                        embed = discord.Embed(title=b.text)
-                        for b in desc:
-                            embed.add_field(name='Description', value=b.text, inline=False)
-                            for b in thumb_url:
-                                embed.set_thumbnail(url=b['src'])
-                                for b in contains:
-                                    embed.set_footer(text="Contains " + b.get_text(separator=' '))
-                                    await ctx.send(embed=embed)
-
-                                
-        except aiohttp.ClientError:
-            await ctx.send("I was unable to get cookies.")
+import aiohttp as E,asyncio as B
+from bs4 import BeautifulSoup as F
+from redbot.core import commands as A
+import discord as G
+class crumbl(A.Cog):
+	def __init__(A,*B,**C):super().__init__(*B,**C);A.__url='https://crumblcookies.com/nutrition';A.__session=E.ClientSession()
+	def cog_unload(A):
+		if A.__session:B.get_event_loop().create_task(A.__session.close())
+	@A.command()
+	async def crumbl(self,ctx):
+		D='class';C=ctx;await C.trigger_typing()
+		try:
+			async with self.__session.get(self.__url)as H:
+				I=await H.text();J=F(I,'html.parser');K=J.find_all('div',class_='bg-white p-5 pb-0 mb-2.5 rounded-lg')
+				for A in K:
+					L=A.find_all('b',{D:'text-lg'});M=A.find_all('p',{D:'text-sm'});N=A.find_all('img',{D:'object-contain'});O=A.find_all('span',{D:'flex items-center justify-center'})
+					for A in L:
+						B=G.Embed(title=A.text)
+						for A in M:
+							B.add_field(name='Description',value=A.text,inline=False)
+							for A in N:
+								B.set_thumbnail(url=A['src'])
+								for A in O:B.set_footer(text='Contains '+A.get_text(separator=' '));await C.send(embed=B)
+		except E.ClientError:await C.send('I was unable to get cookies.')
