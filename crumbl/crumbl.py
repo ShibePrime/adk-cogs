f8eb72b81fea275b9fa56751f3eb988e53f78673
andrewdkennedy1 <andrew@Bortonfruit.com>
2022-12-05 01:38:44 -0800

    more

--- crumbl/crumbl.py
+++ crumbl/crumbl.py
@@ -40,7 +40,7 @@ class crumbl(commands.Cog):
                             for b in thumb_url:
                                 embed.set_thumbnail(url=b['src'])
                                 for b in contains:
-                                    embed.set_footer(text=b.split())
+                                    embed.set_footer(text=b.text.split())
                                     await ctx.send(embed=embed)
 
                                 
