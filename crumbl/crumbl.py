15c29e9b6d855859d146e5220bcb10efddaf9db1
andrewdkennedy1 <andrew@Bortonfruit.com>
2022-12-05 00:09:31 -0800

    crum

--- crumbl/crumbl.py
+++ crumbl/crumbl.py
@@ -27,7 +27,7 @@ class crumbl(commands.Cog):
                 rawingredients = await response.text()
                 soup = BeautifulSoup(rawingredients, "html.parser")
                 cookies = soup.find_all('div', class_="bg-white p-5 pb-0 mb-2.5 rounded-lg")
-                for word in cookies.find_all('b'):
+                for word in cookies.find('b'):
                     crum=word.get_text()
                     await ctx.send(crum)
         except aiohttp.ClientError:
