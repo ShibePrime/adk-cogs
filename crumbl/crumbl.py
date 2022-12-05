bd6242021ff1e916edcdad9d397ec0665564fae9
andrewdkennedy1 <andrew@Bortonfruit.com>
2022-12-05 01:46:33 -0800

    howto make contains fit right

--- crumbl/crumbl.py
+++ crumbl/crumbl.py
@@ -40,7 +40,7 @@ class crumbl(commands.Cog):
                             for b in thumb_url:
                                 embed.set_thumbnail(url=b['src'])
                                 for b in contains:
-                                    embed.set_footer(text=b.text)
+                                    embed.set_footer(text"Contains " + b.text(separator=' '))
                                     await ctx.send(embed=embed)
 
                                 
