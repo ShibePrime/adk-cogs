050d830e3f131cbaa7987a2661f81db9e5624dfe
andrewdkennedy1 <andrew@Bortonfruit.com>
2022-12-08 19:10:33 -0800

    a
    a

--- steamid/steamid.py
+++ steamid/steamid.py
@@ -18,24 +18,30 @@ class steamid(commands.Cog):
         if self.__session:
             asyncio.get_event_loop().create_task(self.__session.close())
 
-    @commands.command()
-    async def steamid(self, ctx: commands.Context) -> None:
-        await ctx.trigger_typing()
+@commands.command()
+async def steamid(self, ctx: commands.Context) -> None:
+    await ctx.trigger_typing()
 
-        try:
-            # Get the response from the URL
-            response = await self.__session.get(self.__url + str(ctx))
+    try:
+        # Get the response from the URL
+        response = await self.__session.get(self.__url + str(ctx.))
 
-            # Get the response's HTML as a string
-            html = await response.text()
+        # Get the response's HTML as a string
+        html = await response.text()
 
-            # Parse the HTML with Beautiful Soup
-            soup = BeautifulSoup(html, 'html.parser')
+        # Parse the HTML with Beautiful Soup
+        soup = BeautifulSoup(html, 'html.parser')
 
-            # Find all div elements with the class '#content > dl'
-            div = soup.find('div', class_='#content')
+        # Find all div elements with the class '#content > dl'
+        div = soup.find('div', class_='#content')
 
-            # Loop through the divs and send their text to the Discord channel
-            await ctx.send(div)
-        except aiohttp.ClientError:
-            await ctx.send("I was unable to get it")
+        # Check if the div exists
+        if div:
+            # If the div exists, send its text to the Discord channel
+            await ctx.send(div.text)
+        else:
+            # If the div doesn't exist, send a different message
+            await ctx.send("empty")
+    except aiohttp.ClientError:
+        # Handle ClientError exceptions
+        await ctx.send("I was unable to get it")
