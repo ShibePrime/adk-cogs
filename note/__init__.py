d02eac21d50cfbda5e5bc3f04da0a71297f5a888
andrewdkennedy1 <andrew@Bortonfruit.com>
2023-01-06 19:41:57 -0800

    Update __init__.py

--- note/__init__.py
+++ note/__init__.py
@@ -3,4 +3,5 @@ from redbot.core.bot import Red
 from .note import note
 
 def setup(bot: Red):
-    bot.add_cog(note())
+    bot.add_cog(note(bot, ctx, "set"))
+    bot.add_cog(note(bot, ctx, "get"))
