3e54f501c7c1675de826e086862b6afe352d8750
andrewdkennedy1 <andrew@Bortonfruit.com>
2023-01-06 22:19:13 -0800

    a
    a

--- note/note.py
+++ note/note.py
@@ -11,6 +11,6 @@ class WriteNote(commands.Cog):
         user_avatar_url = ctx.message.author.avatar_url
 
         # Write the user's name, avatar image link wrapped in <img> HTML tags, and words to the file
-        with open(r"/home/pi/remotescreen/notes.txt", "w") as f:
+        with open(r"pi-deskscreen:/data/compose/mounts/modules/MMM-HTMLSnippet/notes.txt", "w") as f:
             f.write(f"{user_name} <img src='{user_avatar_url}'>: {words}\n")
         await ctx.send("Note written.")
