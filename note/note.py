68b0ddadbe0cf3832619cdf950353be0c50efaab
andrewdkennedy1 <andrew@Bortonfruit.com>
2023-01-06 21:17:27 -0800

    dir

--- note/note.py
+++ note/note.py
@@ -6,14 +6,14 @@ class WriteNote(commands.Cog):
 
     @commands.command()
     async def writenote(self, ctx, *, words: str):
-        with open(r"/home/pi/notes.txt", "w") as f:
+        with open(r"/home/pi/remotescreen/notes.txt", "w") as f:
             f.write(words + "\n")
         await ctx.send("Note written.")
 
     @commands.command()
     async def readnote(self, ctx):
         try:
-            with open(r"/home/pi/notes.txt", "r") as f:
+            with open(r"/home/pi/remotescreen/notes.txt", "r") as f:
                 notes = f.read()
                 if not notes:
                     await ctx.send("No notes have been written.")
