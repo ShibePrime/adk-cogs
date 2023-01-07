62eb0e2593bb0f8b5b496d83b24afc57a69a2e6f
andrewdkennedy1 <andrew@Bortonfruit.com>
2023-01-06 19:56:02 -0800

    a

--- note/__init__.py
+++ note/__init__.py
@@ -1,14 +1,6 @@
 from redbot.core import commands
 
-class WriteNote(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
-    @commands.command()
-    async def writenote(self, ctx, *, words: str):
-        with open(r"/home/pi/notes.txt", "w") as f:
-            f.write(words + "\n")
-        await ctx.send("Note written.")
+from .note import WriteNote
 
 def setup(bot):
     bot.add_cog(WriteNote(bot))
--- note/note.py
+++ note/note.py
@@ -6,6 +6,18 @@ class WriteNote(commands.Cog):
 
     @commands.command()
     async def writenote(self, ctx, *, words: str):
-        with open(r"~\home\pi\notes.txt", "w") as f:
+        with open(r"/home/pi/notes.txt", "w") as f:
             f.write(words + "\n")
         await ctx.send("Note written.")
+
+    @commands.command()
+    async def readnote(self, ctx):
+        try:
+            with open(r"/home/pi/notes.txt", "r") as f:
+                notes = f.read()
+                if not notes:
+                    await ctx.send("No notes have been written.")
+                else:
+                    await ctx.send(notes)
+        except FileNotFoundError:
+            await ctx.send("No notes have been written.")
