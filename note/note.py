5083f8abcb1af5d41e383c51f2f2d71888c74a04
andrewdkennedy1 <andrew@Bortonfruit.com>
2023-01-06 19:43:57 -0800

    a

--- note/note.py
+++ note/note.py
@@ -13,20 +13,7 @@ class note(commands.Cog):
             asyncio.get_event_loop().create_task(self.__session.close())
 
 @commands.command()
-async def note(self, ctx: commands.Context, subcommand: str, *, words: str = None) -> None:
-    if subcommand == "set":
-        with open(r"~\home\pi\notes.txt", "w") as f:
-            f.write(words + "\n")
-            await ctx.send("Notes have been set.")
-    elif subcommand == "get":
-        try:
-            with open(r"~\home\pi\notes.txt", "r") as f:
-                notes = f.read()
-                if not notes:
-                    await ctx.send("No notes have been set.")
-                else:
-                    await ctx.send(notes)
-        except FileNotFoundError:
-            await ctx.send("No notes have been set.")
-    else:
-        await ctx.send("Invalid subcommand. Use `set` or `get`.")
+async def note(self, ctx: commands.Context, *, words: str) -> None:
+    with open(r"~\home\pi\notes.txt", "w") as f:
+        f.write(words + "\n")
+    await ctx.send("Notes have been set.")
