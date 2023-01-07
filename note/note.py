33037c2e3f362d81e5f4e476dfdad2f825758e2a
andrewdkennedy1 <andrew@Bortonfruit.com>
2023-01-06 23:16:45 -0800

    ass

--- note/note.py
+++ note/note.py
@@ -1,6 +1,16 @@
 from redbot.core import commands
 import json
 
+class AssetEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Asset):
+            # Convert the Asset object to a dictionary
+            return {
+                "type": "Asset",
+                "value": obj.toString()
+            }
+        # Let the base class handle other objects
+        return json.JSONEncoder.default(self, obj)
 class WriteNote(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
