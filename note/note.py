31a00498328d998b8d38e78202140a7e055c97ee
Andrew <34286315+andrewdkennedy1@users.noreply.github.com>
2023-01-11 23:58:30 -0800

    emoji handle

--- note/note.py
+++ note/note.py
@@ -13,31 +13,22 @@ class WriteNote(commands.Cog):
             "Authorization": "AIzaSyBjtpGxF9ihX5DY7FCFxTJOc6QTVNvc1b8"
         }
 
-    @commands.command()
-    async def writenote(self, ctx, *, words: str):
-        # Get the user's name and avatar image link
-        user_name = ctx.message.author.name
-        user_avatar_url = ctx.message.author.avatar_url
+@commands.command()
+async def writenote(self, ctx, *, words: str):
+    user_name = ctx.message.author.name
+    user_avatar_url = ctx.message.author.avatar_url
 
-        # Iterate over the message's emojis
-        for emoji in ctx.message.emojis:
-            # Check if the emoji is a custom emoji or an animated emoji
-            if emoji.is_custom_emoji() or emoji.animated:
-                # Replace the emoji with its URL
-                words = words.replace(str(emoji), emoji.url)
-                
-        # Create a dictionary with the user's name, avatar image link, and words
-        data = {
-            "user_name": user_name,
-            "user_avatar_url": str(user_avatar_url),
-            "words": words
-        }
+    words = re.sub(r'<(a)?:[a-zA-Z0-9_]+:([0-9]+)>', lambda x: "<img src='https://cdn.discordapp.com/emojis/{}.{}'>".format(x.group(2), "gif" if x.group(1) else "png"), words)
 
-        # Convert the dictionary to JSON
-        json_data = json.dumps(data)
+    data = {
+        "user_name": user_name,
+        "user_avatar_url": str(user_avatar_url),
+        "words": words
+    }
+    json_data = json.dumps(data)
+    response = requests.post(f"{self.firebase_url}/users.json", headers=self.headers, data=json_data)
 
-        # Send a POST request to the Firebase Realtime Database API
-        response = requests.post(f"{self.firebase_url}/users.json", headers=self.headers, data=json_data)
-
-        if response.status_code == 200:
-            await ctx.send("Note written.")
+    if response.status_code == 200:
+        await ctx.send("Note written.")
+    else:
+        await ctx.send("An error occurred while writing the note.")
