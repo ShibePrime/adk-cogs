e11b9ec37a09f5e619116007740de1be90b4dd48
Andrew <34286315+andrewdkennedy1@users.noreply.github.com>
2023-01-11 22:36:23 -0800

    .

--- note/note.py
+++ note/note.py
@@ -1,9 +1,24 @@
 from redbot.core import commands
 import json
+import pyrebase
 
 class WriteNote(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+        # Initialize Firebase
+        config = {
+          "apiKey": "AIzaSyBjtpGxF9ihX5DY7FCFxTJOc6QTVNvc1b8",
+          "authDomain": "shibeprime-f4fd0.firebaseapp.com",
+          "databaseURL": "https://shibeprime-f4fd0-default-rtdb.firebaseio.com",
+          "projectId": "shibeprime-f4fd0",
+          "storageBucket": "shibeprime-f4fd0.appspot.com",
+          "messagingSenderId": "31986328863",
+          "appId": "1:31986328863:web:cb25dde398d29df667fc2a",
+          "measurementId": "G-746032TVEC"
+        }
+        self.firebase = pyrebase.initialize_app(config)
+        self.db = self.firebase.database()
 
     @commands.command()
     async def writenote(self, ctx, *, words: str):
@@ -17,8 +32,7 @@ class WriteNote(commands.Cog):
             "user_avatar_url": str(user_avatar_url),
             "words": words
         }
-
-        # Write the data to the file as JSON
-        with open(r"/mnt/notes.json", "w") as f:
-            json.dump(data, f)
+        
+        # Push the data to the database
+        self.db.child("users").push(data)
         await ctx.send("Note written.")
