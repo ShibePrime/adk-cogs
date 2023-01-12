72703efe018643436bc52b631342889699d77166
Andrew <34286315+andrewdkennedy1@users.noreply.github.com>
2023-01-12 08:13:07 +0000

    derp

--- note/note.py
+++ note/note.py
@@ -1,7 +1,6 @@
 from redbot.core import commands
 import json
 import requests
-import re
 
 class WriteNote(commands.Cog):
     def __init__(self, bot):
@@ -14,22 +13,26 @@ class WriteNote(commands.Cog):
             "Authorization": "AIzaSyBjtpGxF9ihX5DY7FCFxTJOc6QTVNvc1b8"
         }
 
-@commands.command()
-async def writenote(self, ctx, *, words: str):
-    user_name = ctx.message.author.name
-    user_avatar_url = ctx.message.author.avatar_url
+    @commands.command()
+    async def writenote(self, ctx, *, words: str):
+        # Get the user's name and avatar image link
+        user_name = ctx.message.author.name
+        user_avatar_url = ctx.message.author.avatar_url
 
-    words = re.sub(r'<(a)?:[a-zA-Z0-9_]+:([0-9]+)>', lambda x: "<img src='https://cdn.discordapp.com/emojis/{}.{}'>".format(x.group(2), "gif" if x.group(1) else "png"), words)
+        # Create a dictionary with the user's name, avatar image link, and words
+        data = {
+            "user_name": user_name,
+            "user_avatar_url": str(user_avatar_url),
+            "words": words
+        }
 
-    data = {
-        "user_name": user_name,
-        "user_avatar_url": str(user_avatar_url),
-        "words": words
-    }
-    json_data = json.dumps(data)
-    response = requests.post(f"{self.firebase_url}/users.json", headers=self.headers, data=json_data)
+        # Convert the dictionary to JSON
+        json_data = json.dumps(data)
 
-    if response.status_code == 200:
-        await ctx.send("Note written.")
-    else:
-        await ctx.send("An error occurred while writing the note.")
+        # Send a POST request to the Firebase Realtime Database API
+        response = requests.post(f"{self.firebase_url}/users.json", headers=self.headers, data=json_data)
+
+        if response.status_code == 200:
+            await ctx.send("Note written.")
+        else:
+            await ctx.send("An error occurred while writing the note.")
