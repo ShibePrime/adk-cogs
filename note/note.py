7a6d927eff2d30f594ede751e622b814622b4a29
andrewdkennedy1 <andrew@Bortonfruit.com>
2023-02-06 21:06:26 -0800

    ai did this

--- note/note.py
+++ note/note.py
@@ -1,14 +1,13 @@
-from redbot.core import commands
 import json
-import requests
 import re
+import requests
+
+from redbot.core import commands
 
 
 class WriteNote(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-        # Firebase Realtime Database API endpoint
         self.firebase_url = "https://shibeprime-f4fd0-default-rtdb.firebaseio.com"
         self.headers = {
             "Content-Type": "application/json",
@@ -16,42 +15,25 @@ class WriteNote(commands.Cog):
         }
 
     @commands.command()
-    async def writenote(self, ctx, *, words: str):
-        # Get the user's name and avatar image link
+    async def writenote(self, ctx, *, words):
         user_name = ctx.message.author.display_name
-        user_avatar_url = ctx.message.author.avatar_url
-        # get all mentions in the message
+        user_avatar_url = str(ctx.message.author.avatar_url)
         mentions = ctx.message.mentions
 
-        # replace mentions with the mentioned user's nicknames
         for member in mentions:
-            if member.nick:
-                words = words.replace(f"<@{member.id}>", member.nick)
-            else:
-                words = words.replace(f"<@{member.id}>", member.name)
+            words = words.replace(f"<@{member.id}>", member.nick or member.name)
 
         words = re.sub(
-            r"<(a)?:[a-zA-Z0-9_]+:([0-9]+)>",
+            "<(a)?:[a-zA-Z0-9_]+:([0-9]+)>",
             lambda x: "<img src='https://cdn.discordapp.com/emojis/{}.{}'>".format(
                 x.group(2), "gif" if x.group(1) else "png"
             ),
             words,
         )
 
-        # Create a dictionary with the user's name, avatar image link, and words
-        data = {
-            "user_name": user_name,
-            "user_avatar_url": str(user_avatar_url),
-            "words": words,
-        }
-
-        # Convert the dictionary to JSON
+        data = {"user_name": user_name, "user_avatar_url": user_avatar_url, "words": words}
         json_data = json.dumps(data)
-
-        # Send a POST request to the Firebase Realtime Database API
-        response = requests.post(
-            f"{self.firebase_url}/users.json", headers=self.headers, data=json_data
-        )
+        response = requests.post(f"{self.firebase_url}/users.json", headers=self.headers, data=json_data)
 
         if response.status_code == 200:
             await ctx.send("Note written.")
