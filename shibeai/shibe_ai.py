f84994211ead48d3349287617757b3d0cc77fa54
andrewdkennedy1 <andrew@Bortonfruit.com>
2023-04-07 01:16:07 -0700

    more gpt4 yolo

--- shibeai/shibe_ai.py
+++ shibeai/shibe_ai.py
@@ -1,67 +1,75 @@
-import asyncio
 import json
-import re
+import asyncio
+import websockets
 import random
 import string
-import sys
+from websockets.exceptions import ConnectionClosedOK
 from redbot.core import commands
-import websockets
 
-IP_ADDRESS = "10.101.69.2"
-PORT = "8080"
+server = "10.101.69.2"
+port = "8080"
 
 
-def generate_session_id():
-    charset = string.ascii_lowercase + string.digits
-    return ''.join((random.choice(charset) for _ in range(9)))
+def generate_random_hash():
+    letters = "abcdefghijklmnopqrstuvwxyz0123456789"
+    return ''.join(random.choice(letters) for _ in range(9))
 
 
 class ShibeAI(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.session_id = generate_session_id()
 
-    async def send_request(self, prompt, llm_params):
-        async with websockets.connect(f"ws://{IP_ADDRESS}:{PORT}/queue/join") as websocket:
-            data_list = [
-                prompt,
-                *llm_params.values()
-            ]
-            json_data = {"data": data_list}
+    @commands.command()
+    async def shibeai(self, ctx, *, context: str):
+        session = generate_random_hash()
+        response = await self.process_message(context, session)
+        await ctx.send(response)
 
-            await websocket.send(json.dumps(json_data))
-            response_text = await websocket.recv()
-            print("Response text:", response_text)
-            result = json.loads(response_text)["data"][0]
+    async def process_message(self, context, session):
+        task1 = asyncio.ensure_future(self.send_message_to_fn12(context, session))
+        task2 = asyncio.ensure_future(self.send_message_to_fn13(context, session))
 
-        return result[len(prompt):]
+        await asyncio.gather(task1, task2)
 
-    @commands.command(aliases=["shibeai", "heyshibeai", "hey_shibeai"])
-    async def ask_shibeai(self, ctx, *, question: str):
-        input_text = re.sub(r"@.*\s?", "", question).strip()
-        prompt = f"User: {input_text}\nShibeAI: "
+        return task2.result()
 
-        async with ctx.channel.typing():
-            response_text = await self.send_request(prompt, {
-                "max_new_tokens": 150,
-                "do_sample": True,
-                "temperature": 0.8,
-                "top_p": 0.9,
-                "typical_p": 1,
-                "repetition_penalty": 1.15,
-                "encoder_repetition_penalty": 1.0,
-                "top_k": 100,
-                "min_length": 0,
-                "no_repeat_ngram_size": 0,
-                "num_beams": 1,
-                "penalty_alpha": 0,
-                "length_penalty": 1,
-                "early_stopping": False,
-                "seed": -1
-            })
+    async def send_message_to_fn12(self, context, session):
+        async with websockets.connect(f"ws://{server}:{port}/queue/join") as websocket:
+            while True:
+                content = json.loads(await websocket.recv())
 
-            response_text = response_text.replace("\\", "\\\\").replace("<", "\\<").replace(">", "\\>").replace("@", "\\@ ").strip()
-            if not response_text.strip():
-                response_text = "[ShibeAI Error]"
+                if content["msg"] == "send_hash":
+                    await websocket.send(json.dumps({"session_hash": session, "fn_index": 12}))
+                elif content["msg"] == "send_data":
+                    await websocket.send(json.dumps({"session_hash": session, "fn_index": 12, "data": [context]}))
+                elif content["msg"] == "process_generating":
+                    pass
+                elif content["msg"] == "process_completed":
+                    return True
 
-            await ctx.send(response_text)
+    async def send_message_to_fn13(self, context, session):
+        async with websockets.connect(f"ws://{server}:{port}/queue/join") as websocket:
+            while True:
+                try:
+                    content = json.loads(await websocket.recv())
+                except ConnectionClosedOK:
+                    print("WebSocket connection closed gracefully")
+                    break
+
+                if content["msg"] == "send_hash":
+                    await websocket.send(json.dumps({"session_hash": session, "fn_index": 13}))
+                elif content["msg"] == "send_data":
+                    await websocket.send(json.dumps({"session_hash": session, "fn_index": 13, "data": [
+                        None, 200, True, 1.99, 0.18, 1, 1.15, 1, 30, 0, 0, 1, 0, 1, False, False, "User", "ShibeBot",
+                        "ShibeBot is a helpful assistant that tries its best to help User. ShibeBot never gives any personal information except his name. If User asks for an Image, Picture or Painting of something ShibeBot replies with IMAGE: and the description of the image ALWAYS.",
+                        False, 2048, 1,
+                    ]}))
+                elif content["msg"] == "process_generating":
+                    pass
+                elif content["msg"] == "process_completed":
+                    data = content["output"]["data"]
+                    if "choices" in data[0]:
+                        chosen_model = data[0]["choices"][0]
+                        continue
+
+                    return data[0][-1][-1]
